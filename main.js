35b35f810f832d20a601f17188fb49c24687dc4e
Mr. Robot <glezanthony120@.gm>
2020-03-02 22:38:49 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,96 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+async function searchSongs(term) {
+	const res = await fetch(`${apiURL}/suggest/${term}`);
+	const data = await res.json();
+
+	showData(data);
+}
+
+
+function showData(data) {
+	result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map(
+          song => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+        )
+        .join('')}
+    </ul>
+	`;
+
+	if (data.prev || data.next) {
+		more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')"><i class="fas fa-chevron-circle-left"></i></button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')"><i class="fas fa-chevron-circle-right"></i></button>`
+          : ''
+      }
+    `;
+	} else {
+		more.innerHTML = '';
+	}
+}
+
+async function getMoreSongs(url) {
+	const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+	const data = await res.json();
+
+	showData(data);
+}
+
+async function getLyrics(artist, songTitle) {
+	const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+	const data = await res.json();
+
+	const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+	result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+	more.innerHTML = '';
+}
+
+
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+
+	const searchTerm = search.value.trim();
+
+	if (!searchTerm) {
+		search.classList.remove('search-base');
+		search.classList.add('search-error');
+		search.placeholder = 'Please type in search term...';
+
+	} else {
+		search.classList.remove('search-error');
+		search.classList.add('search-base');
+		search.placeholder = 'Enter Artist or Song...';
+		searchSongs(searchTerm);
+	}
+});
+
+result.addEventListener('click', e => {
+	const clickedEl = e.target;
+
+	if (clickedEl.tagName === 'BUTTON') {
+		const artist = clickedEl.getAttribute('data-artist');
+		const songTitle = clickedEl.getAttribute('data-songtitle');
+
+		getLyrics(artist, songTitle);
+	}
+});
